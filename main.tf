466204b8d7ee31679014bf70e5b37d9ae3b6df74
Brandon O'Connor <brandon.o'connor@ge.com>
2017-12-18 20:12:51 -0800

    moved data source to data file

--- main.tf
+++ main.tf
@@ -21,25 +21,6 @@ resource "aws_alb" "main" {
   depends_on = ["aws_s3_bucket.log_bucket"]
 }
 
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    sid = "AllowToPutLoadBalancerLogsToS3Bucket"
-
-    actions = [
-      "s3:PutObject",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.log_bucket_name}/${var.log_location_prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_elb_service_account.main.id}:root"]
-    }
-  }
-}
-
 resource "aws_s3_bucket" "log_bucket" {
   bucket        = "${var.log_bucket_name}"
   policy        = "${var.bucket_policy == "" ? data.aws_iam_policy_document.bucket_policy.json : var.bucket_policy}"
