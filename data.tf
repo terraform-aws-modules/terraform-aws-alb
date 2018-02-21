f9338c5a609fe4ea9be55f1ab6afb8e6acbdbfca
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2018-02-21 17:46:59 -0600

    fix generated bucket policy if log_location_prefix is omitted (or empty string)

--- data.tf
+++ data.tf
@@ -6,7 +6,7 @@ data "aws_iam_policy_document" "bucket_policy" {
   statement {
     sid       = "AllowToPutLoadBalancerLogsToS3Bucket"
     actions   = ["s3:PutObject"]
-    resources = ["arn:aws:s3:::${var.log_bucket_name}/${var.log_location_prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+    resources = ["arn:aws:s3:::${var.log_bucket_name}/${var.log_location_prefix == "" ? "" : format("%s/", var.log_location_prefix)}AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
 
     principals {
       type        = "AWS"
