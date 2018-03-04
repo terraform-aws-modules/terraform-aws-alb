4830d440894ab0ca16182e75aed3ccd30d24f144
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2018-03-03 19:45:57 -0600

    update force_destroy_log_bucket description (#42)

--- variables.tf
+++ variables.tf
@@ -58,7 +58,7 @@ variable "enable_logging" {
 }
 
 variable "force_destroy_log_bucket" {
-  description = "If set to true and if the log bucket already exists, it will be destroyed and recreated."
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
   default     = false
 }
 
