47d7e36dd1fb60ed0ead59eda43ed8eb3466972c
Nathan Dench <ndench@users.noreply.github.com>
2018-03-04 11:48:46 +1000

    Add target_group.name to outputs.tf (#45)

--- outputs.tf
+++ outputs.tf
@@ -52,3 +52,8 @@ output "target_group_arn" {
   description = "ARN of the target group. Useful for passing to your Auto Scaling group module."
   value       = "${aws_alb_target_group.target_group.arn}"
 }
+
+output "target_group_name" {
+  description = "Name of the target group. Useful for passing to your CodeDeploy Deployment Group."
+  value       = "${aws_alb_target_group.target_group.name}"
+}
