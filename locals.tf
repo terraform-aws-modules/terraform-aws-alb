fa2f74bf73b9e31fcc409253f75c5b41423cb633
Joe Martin <jwmartin83@gmail.com>
2018-09-13 18:18:43 -0400

    merge user-provided target_groups_defaults with our defaults

---
+++ locals.tf
@@ -0,0 +1,17 @@
+locals {
+  target_groups_default_configs = {
+    cookie_duration                  = 86400
+    deregistration_delay             = 300
+    health_check_interval            = 10
+    health_check_healthy_threshold   = 3
+    health_check_path                = "/"
+    health_check_port                = "traffic-port"
+    health_check_timeout             = 5
+    health_check_unhealthy_threshold = 3
+    health_check_matcher             = "200-299"
+    stickiness_enabled               = true
+    target_type                      = "instance"
+  }
+
+  target_groups_defaults = "${merge(local.target_groups_default_configs, var.target_groups_defaults)}"
+}
--- variables.tf
+++ variables.tf
@@ -128,21 +128,7 @@ variable "target_groups_count" {
 
 variable "target_groups_defaults" {
   description = "Default values for target groups as defined by the list of maps."
-  type        = "map"
-
-  default = {
-    "cookie_duration"                  = 86400
-    "deregistration_delay"             = 300
-    "health_check_interval"            = 10
-    "health_check_healthy_threshold"   = 3
-    "health_check_path"                = "/"
-    "health_check_port"                = "traffic-port"
-    "health_check_timeout"             = 5
-    "health_check_unhealthy_threshold" = 3
-    "health_check_matcher"             = "200-299"
-    "stickiness_enabled"               = true
-    "target_type"                      = "instance"
-  }
+  default     = {}
 }
 
 variable "vpc_id" {
