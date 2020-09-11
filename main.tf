03cad596225b568a01dfcee88913b1ad8233d533
David Mattia <8922077+dmattia@users.noreply.github.com>
2020-09-11 06:15:27 -0500

    feat: Added listener rules support (#155)

--- main.tf
+++ main.tf
@@ -110,6 +110,217 @@ resource "aws_lb_target_group" "main" {
   }
 }
 
+resource "aws_lb_listener_rule" "https_listener_rule" {
+  count = var.create_lb ? length(var.https_listener_rules) : 0
+
+  listener_arn = aws_lb_listener.frontend_https[lookup(var.https_listener_rules[count.index], "https_listener_index", count.index)].arn
+  priority     = lookup(var.https_listener_rules[count.index], "priority", null)
+
+  # authenticate-cognito actions
+  dynamic "action" {
+    for_each = [
+      for action_rule in var.https_listener_rules[count.index].actions :
+      action_rule
+      if action_rule.type == "authenticate-cognito"
+    ]
+
+    content {
+      type = action.value["type"]
+      authenticate_cognito {
+        authentication_request_extra_params = lookup(action.value, "authentication_request_extra_params", null)
+        on_unauthenticated_request          = lookup(action.value, "on_authenticated_request", null)
+        scope                               = lookup(action.value, "scope", null)
+        session_cookie_name                 = lookup(action.value, "session_cookie_name", null)
+        session_timeout                     = lookup(action.value, "session_timeout", null)
+        user_pool_arn                       = action.value["user_pool_arn"]
+        user_pool_client_id                 = action.value["user_pool_client_id"]
+        user_pool_domain                    = action.value["user_pool_domain"]
+      }
+    }
+  }
+
+  # authenticate-oidc actions
+  dynamic "action" {
+    for_each = [
+      for action_rule in var.https_listener_rules[count.index].actions :
+      action_rule
+      if action_rule.type == "authenticate-oidc"
+    ]
+
+    content {
+      type = action.value["type"]
+      authenticate_oidc {
+        # Max 10 extra params
+        authentication_request_extra_params = lookup(action.value, "authentication_request_extra_params", null)
+        authorization_endpoint              = action.value["authorization_endpoint"]
+        client_id                           = action.value["client_id"]
+        client_secret                       = action.value["client_secret"]
+        issuer                              = action.value["issuer"]
+        on_unauthenticated_request          = lookup(action.value, "on_unauthenticated_request", null)
+        scope                               = lookup(action.value, "scope", null)
+        session_cookie_name                 = lookup(action.value, "session_cookie_name", null)
+        session_timeout                     = lookup(action.value, "session_timeout", null)
+        token_endpoint                      = action.value["token_endpoint"]
+        user_info_endpoint                  = action.value["user_info_endpoint"]
+      }
+    }
+  }
+
+  # redirect actions
+  dynamic "action" {
+    for_each = [
+      for action_rule in var.https_listener_rules[count.index].actions :
+      action_rule
+      if action_rule.type == "redirect"
+    ]
+
+    content {
+      type = action.value["type"]
+      redirect {
+        host        = lookup(action.value, "host", null)
+        path        = lookup(action.value, "path", null)
+        port        = lookup(action.value, "port", null)
+        protocol    = lookup(action.value, "protocol", null)
+        query       = lookup(action.value, "query", null)
+        status_code = action.value["status_code"]
+      }
+    }
+  }
+
+  # fixed-response actions
+  dynamic "action" {
+    for_each = [
+      for action_rule in var.https_listener_rules[count.index].actions :
+      action_rule
+      if action_rule.type == "fixed-response"
+    ]
+
+    content {
+      type = action.value["type"]
+      fixed_response {
+        message_body = lookup(action.value, "message_body", null)
+        status_code  = lookup(action.value, "status_code", null)
+        content_type = action.value["content_type"]
+      }
+    }
+  }
+
+  # forward actions
+  dynamic "action" {
+    for_each = [
+      for action_rule in var.https_listener_rules[count.index].actions :
+      action_rule
+      if action_rule.type == "forward"
+    ]
+
+    content {
+      type             = action.value["type"]
+      target_group_arn = aws_lb_target_group.main[lookup(action.value, "target_group_index", count.index)].id
+    }
+  }
+
+  # Path Pattern condition
+  dynamic "condition" {
+    for_each = [
+      for condition_rule in var.https_listener_rules[count.index].conditions :
+      condition_rule
+      if length(lookup(condition_rule, "path_patterns", [])) > 0
+    ]
+
+    content {
+      path_pattern {
+        values = condition.value["path_patterns"]
+      }
+    }
+  }
+
+  # Host header condition
+  dynamic "condition" {
+    for_each = [
+      for condition_rule in var.https_listener_rules[count.index].conditions :
+      condition_rule
+      if length(lookup(condition_rule, "host_headers", [])) > 0
+    ]
+
+    content {
+      host_header {
+        values = condition.value["host_headers"]
+      }
+    }
+  }
+
+  # Http header condition
+  dynamic "condition" {
+    for_each = [
+      for condition_rule in var.https_listener_rules[count.index].conditions :
+      condition_rule
+      if length(lookup(condition_rule, "http_headers", [])) > 0
+    ]
+
+    content {
+      dynamic "http_header" {
+        for_each = condition.value["http_headers"]
+
+        content {
+          http_header_name = http_header.value["http_header_name"]
+          values           = http_header.value["values"]
+        }
+      }
+    }
+  }
+
+  # Http request method condition
+  dynamic "condition" {
+    for_each = [
+      for condition_rule in var.https_listener_rules[count.index].conditions :
+      condition_rule
+      if length(lookup(condition_rule, "http_request_methods", [])) > 0
+    ]
+
+    content {
+      http_request_method {
+        values = condition.value["http_request_methods"]
+      }
+    }
+  }
+
+  # Query string condition
+  dynamic "condition" {
+    for_each = [
+      for condition_rule in var.https_listener_rules[count.index].conditions :
+      condition_rule
+      if length(lookup(condition_rule, "query_strings", [])) > 0
+    ]
+
+    content {
+      dynamic "query_string" {
+        for_each = condition.value["query_strings"]
+
+        content {
+          key   = lookup(query_string.value, "key", null)
+          value = query_string.value["value"]
+        }
+      }
+    }
+  }
+
+  # Source IP address condition
+  dynamic "condition" {
+    for_each = [
+      for condition_rule in var.https_listener_rules[count.index].conditions :
+      condition_rule
+      if length(lookup(condition_rule, "source_ips", [])) > 0
+    ]
+
+    content {
+      source_ip {
+        values = condition.value["source_ips"]
+      }
+    }
+  }
+}
+
+
 resource "aws_lb_listener" "frontend_http_tcp" {
   count = var.create_lb ? length(var.http_tcp_listeners) : 0
 
