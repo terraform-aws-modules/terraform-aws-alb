8be7799a43b3b284e617d1df487b5627c35ee63e
florian0410 <florian.silva@hotmail.fr>
2021-07-13 19:58:35 +0200

    add listener rules support for http_tcp

--- variables.tf
+++ variables.tf
@@ -52,6 +52,12 @@ variable "https_listener_rules" {
   default     = []
 }
 
+variable "http_tcp_listener_rules" {
+  description = "A list of maps describing the Listener Rules for this ALB. Required key/values: actions, conditions. Optional key/values: priority, http_tcp_listener_index (default to http_tcp_listeners[count.index])"
+  type        = any
+  default     = []
+}
+
 variable "idle_timeout" {
   description = "The time in seconds that the connection is allowed to be idle."
   type        = number
@@ -149,6 +155,12 @@ variable "target_group_tags" {
 }
 
 variable "https_listener_rules_tags" {
+  description = "A map of tags to add to all https listener rules"
+  type        = map(string)
+  default     = {}
+}
+
+variable "http_tcp_listener_rules_tags" {
   description = "A map of tags to add to all https listener rules"
   type        = map(string)
   default     = {}
