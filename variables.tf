f21e4a1c44037108c4b05f14b74b08e41ead2a60
Anton Babenko <anton@antonbabenko.com>
2022-10-28 13:24:06 +0200

    Reverted var type

--- variables.tf
+++ variables.tf
@@ -30,11 +30,8 @@ variable "enable_cross_zone_load_balancing" {
 
 variable "extra_ssl_certs" {
   description = "A list of maps describing any extra SSL certificates to apply to the HTTPS listeners. Required key/values: certificate_arn, https_listener_index (the index of the listener within https_listeners which the cert applies toward)."
-  type = list(object({
-    https_listener_index = number
-    certificate_arn      = string
-  }))
-  default = []
+  type        = list(map(string))
+  default     = []
 }
 
 variable "https_listeners" {
