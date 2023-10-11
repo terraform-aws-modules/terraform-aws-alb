a6128e4d3a1df69c6b43831ee3e8eac326cca964
Ibrahiem Mohammad <imohammd02@gmail.com>
2023-10-11 12:04:35 -0500

    update lb output descriptions for example

--- examples/complete-alb/outputs.tf
+++ examples/complete-alb/outputs.tf
@@ -1,10 +1,10 @@
 output "lb_id" {
-  description = "The ID and ARN of the load balancer we created."
+  description = "The ID of the load balancer we created."
   value       = module.alb.lb_id
 }
 
 output "lb_arn" {
-  description = "The ID and ARN of the load balancer we created."
+  description = "The ARN of the load balancer we created."
   value       = module.alb.lb_arn
 }
 
